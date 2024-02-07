dd075ba805e17447aaec268567fb378b038ebc98
Nick Meincken <nick@meincken.com>
2024-02-07 14:38:37 +0000

    Speed Insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from "next/app";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "../styles/index.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <Component {...pageProps} />
+    <SpeedInsights />
+    </>
+  );
 }
 
 export default MyApp;
