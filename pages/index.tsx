7e2b6a01c5e20d99af74f1b2942081c012d90227
Nick Meincken <nick@meincken.com>
2024-02-07 15:03:09 +0000

    WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,25 +9,28 @@ export default function Index({ allPosts: { edges } }) {
   const morePosts = edges.slice(1);
 
   return (
-    <Layout>
+    <>
       <Head>
-        <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+        <title>{CMS_NAME}</title>
       </Head>
-      <Container>
-        <Intro />
-        {heroPost && (
-          <HeroPost
-            title={heroPost.title}
-            coverImage={heroPost.featuredImage}
-            date={heroPost.date}
-            author={heroPost.author}
-            slug={heroPost.slug}
-            excerpt={heroPost.excerpt}
-          />
-        )}
-        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-      </Container>
-    </Layout>
+      <Intro />
+      <Layout>
+
+        <Container>
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.featuredImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
+      </Layout>
+    </>
   );
 }
 
