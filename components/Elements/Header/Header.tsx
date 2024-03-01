074616fe8e2ccbc2aaaff03916dc7a8bf10e8067
Nick Meincken <nick@meincken.com>
2024-03-01 18:22:47 +0000

    test modules

--- components/Elements/Header/Header.tsx
+++ components/Elements/Header/Header.tsx
@@ -1,5 +1,5 @@
 // import Link from "next/link";
-import styles from './Header.module.css'
+import styles from './Header.modules.css'
 import { CMS_NAME } from "@/lib/constants"
 
 export const Header = () => {
